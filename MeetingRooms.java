0f95a756fb9e93181dd7c7c5819ac9d1a14c3ea6
AdityaDhanraj3 <shivkaradityadhanraj@gmail.com>
2020-11-07 08:52:29 +0530

    MeetingRooms code

---
+++ MeetingRooms.java
@@ -0,0 +1,45 @@
+package project1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MeetingRooms {
+	public class Interval{
+		int start;
+		int end;
+		Interval(int start, int end){
+			start = 0;
+			end = 0;
+		}
+		
+	}
+	public static boolean canAttendMeetings(Interval intervals[]) {
+		int[] starts = new int[intervals.length];
+		int[] ends = new int[intervals.length];
+		for(int i=0;i<intervals.length;i++) {
+			starts[i] = intervals[i].start;
+			ends[i] = intervals[i].end;
+		}
+		Arrays.sort(starts);
+		Arrays.sort(ends);
+		for(int i=0;i<starts.length-1;i++) {
+			if(starts[i+1]<ends[i]) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+
+	public static void main(String[] args) {
+		/*
+		 * MeetingRooms mr = new MeetingRooms(); Interval[] intervals = new Interval[3];
+		 * intervals[0] = new Interval(0,10); intervals[1] = new Interval(1,3);
+		 * intervals[2] = new Interval(2,6); canAttendMeetings(intervals);
+		 */
+		int starts[] = {0,1,2};
+		int ends[] = {10,3,6};
+		
+	}
+
+}
