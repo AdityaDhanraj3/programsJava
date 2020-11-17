382c3be6d779c501dbbacecb45ae37eb1821c7bc
AdityaDhanraj3 <shivkaradityadhanraj@gmail.com>
2020-11-17 17:56:58 +0530

    formattedIsSubString

--- IsSubString.java
+++ IsSubString.java
@@ -1,5 +1,16 @@
 package project1;
+/**
+ * Given two strings s and t, return the minimum window in s which will contain all the characters in t. 
+ * If there is no such window in s that covers all characters in t, return the empty string "".
 
+Note that If there is such a window, it is guaranteed that there will always be only one unique minimum window in s.
+Example 1:
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BAC"
+ * @author adity
+ *
+ */
 public class IsSubString {
 	public static int isSubString(String str1, String str2) {
 		int m = str1.length();
@@ -19,7 +30,6 @@ public class IsSubString {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String str1 = "XYZ";
 		String str2 = "ABXYZC";
 		System.out.println(isSubString(str1, str2));
