55343ecd8287b9a0c580bf1c5bc894b0fab8f28b
AdityaDhanraj3 <shivkaradityadhanraj@gmail.com>
2020-11-17 18:07:48 +0530

    formattedPartitionLabel

--- PartitionLabel.java
+++ PartitionLabel.java
@@ -1,5 +1,18 @@
 package project1;
+/**
+ * A string S of lowercase English letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
 
+ 
+
+Example 1:
+
+Input: S = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+ */
 import java.util.ArrayList;
 import java.util.List;
 
