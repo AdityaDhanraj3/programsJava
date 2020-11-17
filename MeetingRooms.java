1b6efc56508297c399aecc0a18e91597fbb55c3e
AdityaDhanraj3 <shivkaradityadhanraj@gmail.com>
2020-11-17 18:04:18 +0530

    formattedMeetingRooms

--- MeetingRooms.java
+++ MeetingRooms.java
@@ -1,5 +1,11 @@
 package project1;
+/**
+ * Given an array of meeting time intervals consisting of start and end times [s1, e1], [s2, e2], ... , determine if a person could attend all meetings.
 
+For example,
+Given [ [0, 30], [5, 10], [15, 20] ],
+return false.
+ */
 import java.util.ArrayList;
 import java.util.Arrays;
 
