6a3bc359d9ab4b227e0c600055b101c8d151de48
AdityaDhanraj3 <shivkaradityadhanraj@gmail.com>
2020-11-17 17:47:33 +0530

    formattedFindTheSmallestWindowToBeSorted

--- FindTheSmallestWindowToBeSorted.java
+++ FindTheSmallestWindowToBeSorted.java
@@ -1,5 +1,17 @@
 package project1;
+/**
+ * Given an unsorted array arr[0..n-1] of size n, find the minimum length subarray arr[s..e] such that sorting this
+ * subarray makes the whole array sorted.
 
+Examples:
+1) If the input array is [10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60], your program should be able to find that the subarray 
+   lies between the indexes 3 and 8.
+
+2) If the input array is [0, 1, 15, 25, 6, 7, 30, 40, 50], your program should be able to find that the subarray
+  lies between the indexes 2 and 5.
+ * @author adity
+ *
+ */
 public class FindTheSmallestWindowToBeSorted {
 	public static void findSmallestWindow(int arr[]) {
 		int leftIndex = -1;
@@ -15,7 +27,7 @@ public class FindTheSmallestWindowToBeSorted {
 				rightIndex = i;
 			}
 		}
-		// traverse from right to left and keep track of minimum so far.
+		// traverse from right to left and keep track of minimum so far. 
 		int minSoFar = Integer.MAX_VALUE;
 		for (int i = arr.length - 1; i >= 0; i--) {
 			if (minSoFar > arr[i]) {
