808bba01e27f81a72accabeb3c2b753b01159017
AdityaDhanraj3 <shivkaradityadhanraj@gmail.com>
2020-11-21 22:46:11 +0530

    formattedMyBinarySearch

--- MyBinarySearch.java
+++ MyBinarySearch.java
@@ -1,5 +1,21 @@
 package project2;
+/**
+ * A binary search or half-interval search algorithm finds the position of a specified value (the input "key") within a sorted array. In each step, the algorithm compares the input key value with the key value of the middle element of the array. If the keys match, then a matching element has been found so its index, or position, is returned. Otherwise, if the sought key is less than the middle element's key, then the algorithm repeats its action on the sub-array to the left of the middle element or, if the input key is greater, on the sub-array to the right. If the remaining array to be searched is reduced to zero, then the key cannot be found in the array and a special "Not found" indication is returned.
 
+Every iteration eliminates half of the remaining possibilities. This makes binary searches very efficient - even for large collections.
+
+Binary search requires a sorted collection. Also, binary searching can only be applied to a collection that allows random access (indexing).
+
+Worst case performance: O(log n)
+
+Best case performance: O(1)
+
+Recursion is used in this algorithm because with each pass a new array is created by cutting the old one in half. The binary search procedure is then called recursively, this time on the new array. Typically the array's size is adjusted by manipulating a beginning and ending index. The algorithm exhibits a logarithmic order of growth because it essentially divides the problem domain in half with each pass.
+
+
+ * @author adity
+ *
+ */
 public class MyBinarySearch {
 	public int binarySearch(int[] inputArr, int key) {
 		int start = 0;
