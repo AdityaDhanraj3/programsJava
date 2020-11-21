70c86a7126564c781738bd8689ce9393fed7ed5a
AdityaDhanraj3 <shivkaradityadhanraj@gmail.com>
2020-11-21 22:29:42 +0530

    formattedAnagram

--- Anagram.java
+++ Anagram.java
@@ -1,13 +1,27 @@
 package project2;
+/**
+ * Given two strings s and t , write a function to determine if t is an anagram of s.
 
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+Note:
+You may assume the string contains only lowercase alphabets.
+ */
 import java.util.Arrays;
 
 public class Anagram {
 	public static boolean isAnagram(String str1, String str2) {
 		int st1Len = str1.length();
 		int st2Len = str2.length();
-		if (st1Len != st2Len)
-			return false;
+		/*
+		 * if (st1Len != st2Len) return false;
+		 */
 		char[] chArr = str1.toCharArray();
 		Arrays.sort(chArr);
 		System.out.println(chArr);
@@ -20,11 +34,10 @@ public class Anagram {
 			}
 		}
 		return true;
-	}
+	} 
 
 	public static void main(String[] args) {
 
 		System.out.println(isAnagram("listen", "silent"));
 	}
-
 }
