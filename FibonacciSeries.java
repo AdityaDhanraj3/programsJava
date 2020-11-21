aee1fb39fb2225f9391511bb1b4d064025d44804
AdityaDhanraj3 <shivkaradityadhanraj@gmail.com>
2020-11-21 22:43:46 +0530

    formattedFibonacciSeries

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,4 +1,15 @@
 package project2;
+/**
+ * Note : repetition of number is allowed.
+Example:
+N = 4
+Fibonacci numbers : 1 1 2 3 5 .... so on
+here 2 + 2 = 4
+
+so minimum numbers will be 2
+ * @author adity
+ *
+ */
 
 public class FibonacciSeries {
 
