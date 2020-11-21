f0e8d19179cc2cf4459219c4761a3929c3f15536
AdityaDhanraj3 <shivkaradityadhanraj@gmail.com>
2020-11-21 22:39:16 +0530

    formattedBstMinMaxValues

--- BstMinMaxValues.java
+++ BstMinMaxValues.java
@@ -1,5 +1,13 @@
 package project2;
-
+/**
+ * Description:
+For a binary tree to be a binary search tree (BST), the data of all the nodes in the left sub-tree of
+ the root node should be less than or equals to the data of the root.
+ The data of all the nodes in the right subtree of the root node should be greater than the data of the root.
+ This example shows how to find min & max value of a binary search tree.
+ * @author adity
+ *
+ */
 public class BstMinMaxValues {
 	private BstNode root = null;
 
