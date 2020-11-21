24a0efa25e932d9da21bf4415f05fb72573fccde
AdityaDhanraj3 <shivkaradityadhanraj@gmail.com>
2020-11-21 22:34:38 +0530

    formattedBalancedExpression

--- BalancedExpression.java
+++ BalancedExpression.java
@@ -1,5 +1,53 @@
 package project2;
+/**
+ * A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
 
+Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {) occurs to the left 
+of a closing bracket (i.e., ), ], or }) of the exact same type. There are three types of
+ matched pairs of brackets: [], {}, and ().
+
+A matching pair of brackets is not balanced if the set of brackets it encloses are not matched.
+ For example, {[(])} is not balanced because the contents in between { and } are not balanced.
+  The pair of square brackets encloses a single, unbalanced opening bracket, (, and the 
+  pair of parentheses encloses a single, unbalanced closing square bracket, ].
+
+
+ 
+By this logic, we say a sequence of brackets is balanced if the following conditions are met:
+It contains no unmatched brackets.
+The subset of brackets enclosed within the confines of a matched pair of brackets
+ is also a matched pair of brackets.
+Given n strings of brackets, determine whether each sequence of brackets is balanced.
+ If a string is balanced, return YES. Otherwise, return NO.
+
+Sample Input:
+?
+1
+2
+3
+4
+3
+{[()]}
+{[(])}
+{{[[(())]]}}
+Sample Output:
+?
+1
+2
+3
+YES
+NO
+YES
+Our Approach
+
+1) Declare a stack.
+
+2) Now traverse the expression string character by character.
+
+If the current character is a opening bracket (‘(‘ or ‘{‘ or ‘[‘) then push it to stack.
+If the current character is a closing bracket (‘)’ or ‘}’ or ‘]’) then pop character from stack and if the popped character is the matching opening bracket then fine else parenthesis are not balanced.
+3) After complete traversal, if there the stack is not empty, then the expression is not balanced.
+ */
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
