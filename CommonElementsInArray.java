6f96882bacfc14993e9d5723c4b0860351398984
AdityaDhanraj3 <73998556+AdityaDhanraj3@users.noreply.github.com>
2020-12-17 18:03:27 +0530

    Update CommonElementsInArray.java

--- CommonElementsInArray.java
+++ CommonElementsInArray.java
@@ -23,10 +23,16 @@ public class CommonElementsInArray {
         int[] arr2 = {3,2,12,9,40,32,4};
         for(int i=0;i<arr1.length;i++){
             for(int j=0;j<arr2.length;j++){
-                if(arr1[i]==arr2[j]){
+                if(arr1[i]==arr2[j])
                     System.out.println(arr1[i]);
-                }
             }
         }
     }
 }
+
+OUTPUT : 
+4
+3
+9
+2
+
